c58e1a9b40bbcf13c144a90c011e1c7bd91aaeed
luckyDaveKim <lucky_dave@naver.com>
2021-09-15 12:40:46 +0900

    feat: Add volume stock chart

--- src/components/organisms/Dashboard/Dashboard.tsx
+++ src/components/organisms/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import CompanySelect from "../../atoms/CompanySelect/CompanySelect";
 import ChartDay from "../../molecules/ChartDay/ChartDay";
 import React from "react";
+import VolumeStockChart from "../../atoms/VolumeStockChart/VolumeStockChart";
 
 function Dashboard() {
   return (
@@ -488,6 +489,59 @@ function Dashboard() {
                   <div className="row">
                     <div className="col-lg-9">
                       <ChartDay/>
+                    </div>
+                    <div className="col-lg-3">
+                      <ul className="stat-list">
+                        <li>
+                          <h2 className="no-margins">2,346</h2>
+                          <small>Total orders in period</small>
+                          <div className="stat-percent">48% <i className="fa fa-level-up text-navy"></i></div>
+                          <div className="progress progress-mini">
+                            <div style={{'width': '48%'}} className="progress-bar"></div>
+                          </div>
+                        </li>
+                        <li>
+                          <h2 className="no-margins ">4,422</h2>
+                          <small>Orders in last month</small>
+                          <div className="stat-percent">60% <i className="fa fa-level-down text-navy"></i></div>
+                          <div className="progress progress-mini">
+                            <div style={{'width': '60%'}} className="progress-bar"></div>
+                          </div>
+                        </li>
+                        <li>
+                          <h2 className="no-margins ">9,180</h2>
+                          <small>Monthly income from orders</small>
+                          <div className="stat-percent">22% <i className="fa fa-bolt text-navy"></i></div>
+                          <div className="progress progress-mini">
+                            <div style={{'width': '22%'}} className="progress-bar"></div>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="ibox ">
+                <div className="ibox-title">
+                  <h5>Orders</h5>
+                  <div className="ibox-tools">
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-xs btn-white active">Today</button>
+                      <button type="button" className="btn btn-xs btn-white">Monthly</button>
+                      <button type="button" className="btn btn-xs btn-white">Annual</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="ibox-content">
+                  <div className="row">
+                    <div className="col-lg-9">
+                      <VolumeStockChart/>
                     </div>
                     <div className="col-lg-3">
                       <ul className="stat-list">
