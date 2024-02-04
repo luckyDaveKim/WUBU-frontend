032983dc0f723a6a9d1381f40b266f7bd822f853
mingyu.k <mingyu.k@navercorp.com>
2024-02-04 18:47:49 +0900

    feat: Add rangeDate picker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
-import CandlestickDailyPriceChart from './page/CandlestickDailyPriceChart';
-import CompanySelector from './components/CompanySelector';
 import { useState } from 'react';
 
-const getDailyPriceUri = (companyCode) => {
-    return `http://localhost:8000/api/companies/${companyCode}/price/daily`;
+import CandlestickDailyPriceChart from './page/CandlestickDailyPriceChart';
+import CompanySelector from './components/CompanySelector';
+import RangeDatePicker from './components/RangeDatePicker';
+
+const getDailyPriceUri = (companyCode, startDate, endDate) => {
+    return `http://localhost:8000/api/companies/${companyCode}/price/daily?startDate=${startDate}&endDate=${endDate}`;
 };
 
 function App() {
     const [selectedCompany, setSelectedCompany] = useState('');
+    const [selectedRangeDate, setSelectedRnageDate] = useState({});
 
     const onChangeCompany = ({ target }) => {
         setSelectedCompany(target.value);
     };
 
+    const onChangeRangeDate = ({ startDate, endDate }) => {
+        setSelectedRnageDate({ startDate, endDate });
+    };
+
     return (
         <div className="App">
             <CompanySelector onChangeCompany={onChangeCompany} />
-            {!!selectedCompany && (
-                <CandlestickDailyPriceChart uri={getDailyPriceUri(selectedCompany)} />
-            )}
+            <RangeDatePicker onChangeRangeDate={onChangeRangeDate} />
+            {!!selectedCompany
+                && !!selectedRangeDate?.startDate && !!selectedRangeDate?.endDate
+                && (
+                    <CandlestickDailyPriceChart uri={getDailyPriceUri(selectedCompany, selectedRangeDate?.startDate, selectedRangeDate?.endDate)} />
+                )}
         </div>
     );
 }
---
+++ src/components/RangeDatePicker.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+
+const DATE_FORMAT = 'YYYY-MM-DD';
+const DEFAULT_MONTH_RANGE = 3;
+const RangeDatePicker = ({ onChangeRangeDate }) => {
+    const today = dayjs();
+    const lastMonthDay = today.subtract(DEFAULT_MONTH_RANGE, 'month');
+
+    const [startDate, setStartDate] = useState(lastMonthDay.format(DATE_FORMAT));
+    const [endDate, setEndDate] = useState(today.format(DATE_FORMAT));
+
+    const onChangeStartDate = ({ target }) => {
+        setStartDate(target.value);
+    };
+    const onChangeEndDate = ({ target }) => {
+        setEndDate(target.value);
+    };
+
+    useEffect(() => {
+        if (!startDate || !endDate) {
+            return;
+        }
+
+        if (!dayjs(startDate).isBefore(dayjs(endDate))) {
+            throw new Error(`시작 값이 종료 값보다 큽니다!!! startDate: ${startDate}, endDate: ${endDate}`);
+        }
+
+        onChangeRangeDate({ startDate, endDate });
+    }, [startDate, endDate]);
+
+    return (
+        <div>
+            <input type="date" value={startDate} onChange={onChangeStartDate} />
+            ~
+            <input type="date" value={endDate} onChange={onChangeEndDate} />
+        </div>
+    );
+};
+
+export default RangeDatePicker;
