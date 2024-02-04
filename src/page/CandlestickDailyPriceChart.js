ec5d3a262e5f23c20bc8dd3084ce44b4129f2461
mingyu.k <mingyu.k@navercorp.com>
2024-02-04 16:37:58 +0900

    feat: Add apexcharts chart

---
+++ src/page/CandlestickDailyPriceChart.js
@@ -0,0 +1,28 @@
+import CandlestickChart from './CandlestickChart';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const CandlestickDailyPriceChart = ({ uri }) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get(uri)
+            .then(res => {
+                setData(res.data.map(eachData => {
+                    return {
+                        x: new Date(eachData.date_timestamp),
+                        y: eachData.price
+                    };
+                }));
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, [uri]);
+
+    return (
+        <CandlestickChart data={data} />
+    );
+};
+
+export default CandlestickDailyPriceChart;
